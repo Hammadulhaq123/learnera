194a93a0ede5137f74e97fd016c0bffbd229b5de
Hammadulhaq123 <professorcoding123@gmail.com>
2023-02-17 19:20:10 +0500

    Developed data.js to fetch card info from it

---
+++ src/utils/data.js
@@ -0,0 +1,173 @@
+export const comments = [
+    {
+      name: "PHP",
+      description: "A web server programming language",
+      button: "Learn PHP",
+      bg: "#ffc0c7"
+    },
+    {
+      name: "Jquery",
+      description: "A JS library for developing web pages",
+      button: "Learn Jquery",
+      bg: "#fff4a3"
+    },
+    {
+      name: "Java",
+      description: "A programming language",
+      button: "Learn Java",
+      bg: "#f3ecea"
+    },
+    {
+      name: "C++",
+      description: "A programming language",
+      button: "Learn C++",
+      bg: "#d9eee1"
+    },
+    {
+      name: "Tailwind",
+      description: "A css framework for inline styling",
+      button: "Learn Tailwind",
+      bg: "#96d4d4"
+    },
+    {
+        name: "Bootstrap",
+        description: "A css framework for inline styling",
+        button: "Learn Bootstrap",
+        bg: "#e7e9eb"
+      },
+  ];
+
+
+
+
+export const langs = [
+    {
+        name: "R",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Kotlin",
+        bg: "#d9eee1",
+    },
+    {
+        name: "Node.js",
+        bg: "#96d4d4",
+    },
+    {
+        name: "React",
+        bg: "#ffc0c7",
+    },
+    {
+        name: "JSON",
+        bg: "#96d4d4",
+    },
+    {
+        name: "AngularJS",
+        bg: "#fff4a3",
+    },
+    {
+        name: "MySQL",
+        bg: "#d9eee1",
+    },
+    {
+        name: "XML",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Sass",
+        bg: "#d9eee1",
+    },
+    {
+        name: "Icons",
+        bg: "#96d4d4",
+    },
+    {
+        name: "RWD",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Graphics",
+        bg: "#fff4a3",
+    },
+    {
+        name: "SVG",
+        bg: "#96d4d4",
+    },
+    {
+        name: "Canva",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Rasberry Pi",
+        bg: "#96d4d4",
+    },
+    {
+        name: "Cyber Security",
+        bg: "#d9eee1",
+    },
+    {
+        name: "Colors",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Git",
+        bg: "#96d4d4",
+    },
+    {
+        name: "Matplotlib",
+        bg: "#fff4a3",
+    },
+    {
+        name: "Numpy",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Pandas",
+        bg: "#d9eee1",
+    },
+    {
+        name: "SciPy",
+        bg: "#f3ecea",
+    },
+    {
+        name: "ASP",
+        bg: "#fff4a3",
+    },
+    {
+        name: "Accesibility",
+        bg: "#d9eee1",
+    },
+    {
+        name: "AppML",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Go",
+        bg: "#fff4a3",
+    },
+    {
+        name: "Typescript",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Django",
+        bg: "#96d4d4",
+    },
+    {
+        name: "MongoDB",
+        bg: "#d9eee1",
+    },
+    {
+        name: "Statisitics",
+        bg: "##fff4a3",
+    },
+    {
+        name: "Data Science",
+        bg: "#f3ecea",
+    },
+    {
+        name: "Typing Speed",
+        bg: "#96d4d4",
+    },
+
+];
