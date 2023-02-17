9ea954e67f5611f257b58c87d0d4ecc54b5a9e9d
Hammadulhaq123 <professorcoding123@gmail.com>
2023-02-17 19:21:37 +0500

    Developing home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Header from './components/header/Header';
 import React, { useState } from "react";
 import Hero from "./components/hero/Hero";
 import Html from "./components/html/Html";
+import Css from "./components/css/Css";
+import Js from "./components/js/Javascript";
+import Python from "./components/python/Python";
+import Sql from "./components/sql/Sql";
+import Biggies from "./components/biggies/Biggies";
+import Medium from "./components/medium/Medium";
+import Lang from "./components/langs/Lang"
+import Endcont from "./components/endcont/Endcont"
 
 function App() {
   const [theme, setTheme] = useState("dark");
@@ -20,6 +28,14 @@ function App() {
         <Header theme={theme} toggleTheme={toggleTheme}/>
         <Hero theme={theme} />
         <Html theme={theme} />
+        <Css theme={theme} />
+        <Js theme={theme} />
+        <Python theme={theme} />
+        <Sql theme={theme} />
+        <Biggies theme={theme} />
+        <Medium theme={theme} />
+        <Lang theme={theme} />
+        <Endcont theme={theme} />
       </div>
     </>
   );
